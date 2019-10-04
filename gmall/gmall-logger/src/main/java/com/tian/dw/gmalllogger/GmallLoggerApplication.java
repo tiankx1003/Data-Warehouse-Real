43ef4236347c43596f2320b63e611ba66f994fac
tiankx1003 <tiankx1003@gmail.com>
2019-10-04 15:56:54 +0800

    settings

--- gmall/gmall-logger/src/main/java/com/tian/dw/gmalllogger/GmallLoggerApplication.java
+++ gmall/gmall-logger/src/main/java/com/tian/dw/gmalllogger/GmallLoggerApplication.java
@@ -6,12 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GmallLoggerApplication {
 /*
-
-java -cp ./gmall-logger-0.0.1-SNAPSHOT.jar:/opt/module/scala-2.11.8/lib/scala-library org.springframework.boot.loader.JarLauncher
+java -cp /opt/gmall/jars/gmall-logger-0.0.1-SNAPSHOT.jar:$SCALA_HOME/lib/scala-library.jar org.springframework.boot.loader.JarLauncher
 java -Djava.ext.dirs=/opt/module/scala-2.11.8/lib -cp ./gmall-logger-0.0.1-SNAPSHOT.jar org.springframework.boot.loader.JarLauncher
  */
     public static void main(String[] args) {
         SpringApplication.run(GmallLoggerApplication.class, args);
     }
-
 }
