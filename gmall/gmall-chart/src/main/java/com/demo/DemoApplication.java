23f91185ad4ea0070bafef44d65bbea498e6563b
tiankx1003 <tiankx1003@gmail.com>
2019-10-09 15:39:35 +0800

    gmall chart

--- gmall/gmall-chart/src/main/java/com/demo/DemoApplication.java
+++ gmall/gmall-chart/src/main/java/com/demo/DemoApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class}) // TODO: 2019/10/9 注解报错
 public class DemoApplication {
-	// TODO: 2019/10/9 注解报错
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
